d5dd6c3acb13e59206bbf28f892089c1224a7894
yo momma <shutup@oops.wtf>
2026-01-21 04:49:14 +0000

    test: dashboard Settings opens

---
+++ platform/android/tests/androidTest/java/org/opendroidpdf/SettingsLaunchInstrumentedTest.kt
@@ -0,0 +1,26 @@
+package org.opendroidpdf
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SettingsLaunchInstrumentedTest {
+
+    @Test
+    fun tappingSettingsFromDashboardOpensSettingsScreen() {
+        ActivityScenario.launch(OpenDroidPDFActivity::class.java).use {
+            onView(withId(R.id.menu_settings)).check(matches(isDisplayed()))
+            onView(withId(R.id.menu_settings)).perform(click())
+            onView(withText(R.string.autosave_settings)).check(matches(isDisplayed()))
+        }
+    }
+}
+
